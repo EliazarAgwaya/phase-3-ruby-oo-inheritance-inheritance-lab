0ebd810b9bcb4ec0e3286f10528c55d89af420de
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-09-05 05:43:54 +0300

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
+    def initialize(knowledge)
+        @knowledge=[]
+    end
+  
+    def learn str
+        @knowledge << str
+    end
 
+    def self.knowledge
+        @knowledge
+    end
 end
